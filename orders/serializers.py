7809f30b327aa0dfc6eeabe661f9bb46da1197e5
Mannuru Nikhitha <mannurunikhitha@gmail.com>
2026-03-26 20:04:21 +0530

    68268

--- orders/serializers.py
+++ orders/serializers.py
@@ -7,7 +7,7 @@ class OrderItemSerializer(serializers.ModelSerializer):
         fields = ['product_name', 'quantity', 'price']
 
 class OrderSerializer(serializers.ModelSerializer):
-    items = OrderItemSerializer(many=True)
+    items = OrderItemSerializer(many=True, read_only=True)
 
     class Meta:
         model = Order
