7b04d004249bc19bf7b2e7c1d65c946f7efa65fe
Mannuru Nikhitha <mannurunikhitha@gmail.com>
2026-03-26 20:49:31 +0530

    68269

--- home/models.py
+++ home/models.py
@@ -1,21 +1,22 @@
 from django.db import models
-
 # Create your models here.
 class MenuCategory(models.Model):
     name=models.CharField(max_length=100,unique=True)
     def __str__(self):
         return self.name
-    
+
 class Restaurant(models.Model):
     name=models.CharField(max_length=255)
     address=models.TextField()
     has_delivery=models.BooleanField(default=False)
     def __str__(self):
         return self.name
-    
-class DailyOperatingHours(models.Model):
-    day = models.CharField(max_length=10)
-    open_time = models.TimeField()
-    close_time = models.TimeField()
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_length=10, decimal_places=2)
+    description = models.TextField(blank=True)
+    is_featured = models.BooleanField(default=False)
+
     def __str__(self):
-        return f"{self.day}: {self.open_time} - {self.close_time}"
+        return self.name
