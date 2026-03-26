18e197ab6aa80ce0a06e91838551a5fcaa87a418
Mannuru Nikhitha <mannurunikhitha@gmail.com>
2026-03-26 20:51:18 +0530

    68269

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
 class OrderStatus(models.Model):
@@ -9,15 +8,15 @@ class OrderStatus(models.Model):
 
 class Order(models.Model):
     status = models.ForeignKey(OrderStatus,on_delete=models.SET_NULL,null=True,blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True,null=True,blank=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2,null=True,blank=True)
     def __str__(self):
         return f'Order #{self.id} - Status: {self.status}'
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
-    product_name = models.CharField(max_length=255)
+    product_name = models.CharField(max_digits=255)
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
 
