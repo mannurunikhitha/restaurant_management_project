8b60ef4541bf961a7c8a24fd022be1c4188bd168
Mannuru Nikhitha <mannurunikhitha@gmail.com>
2026-03-30 23:28:52 +0530

    68644

--- home/models.py
+++ home/models.py
@@ -17,7 +17,7 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_length=10, decimal_places=2)
     description = models.TextField(blank=True)
     is_featured = models.BooleanField(default=False)
-    Ingredient = models.ManyToManyField(Ingredient, related_name="menu_items")
+    Ingredient = models.ManyToManyField('Ingredient', related_name="menu_items")
     def __str__(self):
         return self.name
 
