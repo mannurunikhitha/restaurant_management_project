490f8531bc9975d3c3c49ba85e14d42401e75409
Mannuru Nikhitha <mannurunikhitha@gmail.com>
2026-04-04 23:41:25 +0530

    69131

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+def mark_orders_processed(modeladmin, request, queryset):
+    queryset.update(status='Processed')
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'status']
+    actions = [mark_orders_processed]
+
+admin.site.register(Order, OrderAdmin)
