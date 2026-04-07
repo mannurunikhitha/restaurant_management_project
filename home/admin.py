b30cad3c37539717581a4f8ba89a21b46a91316d
Mannuru Nikhitha <mannurunikhitha@gmail.com>
2026-04-07 20:28:27 +0530

    69261

--- home/admin.py
+++ home/admin.py
@@ -9,3 +9,24 @@ admin.site.register(Restaurant)
 admin.site.register(DailyOperatingHours)
 
 admin.site.register(MenuCategory)
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address', 'phone_number', 'email', 'is_active']
+    search_fields = ['name', 'address']
+    list_filter = ['is_active']
+    ordering = ['name']
+    list_per_page = 10
+    list_display_links = ['name']
+    list_editable = ['is_active']
+    fieldsets = (
+        ('Basic Info', {
+            'fields': ('name', 'address')
+        }),
+        ('Contact Info', {
+            'fields': ('phone_number', 'email')
+        }),
+        ('Status', {
+            'fields': ('is_active',)
+        }),
+    )
