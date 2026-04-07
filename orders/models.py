5172c647cafad07369d4cc26c1b4eca652cbc23a
Mannuru Nikhitha <mannurunikhitha@gmail.com>
2026-04-07 20:36:37 +0530

    69262

--- orders/models.py
+++ orders/models.py
@@ -19,6 +19,12 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True,null=True,blank=True)
     total_price = models.DecimalField(max_digits=10, decimal_places=2,null=True,blank=True)
     objects = OrderManager()
+    def get_unique_item_names(self):
+        item_names = set()
+        for item in self.items.all():
+            item_names.add(item.menu_item.name)
+        return list(item_names)
+        
     def __str__(self):
         return f'Order #{self.id} - Status: {self.status}'
 
@@ -27,6 +33,7 @@ class OrderItem(models.Model):
     product_name = models.CharField(max_digits=255)
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
+    menu_item = models.ForeignKey(Order, on_delete=models.CASCADE)
 
 class OrderManager(models.Manager):
     def get_active_orders(self):
