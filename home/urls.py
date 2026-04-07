91320542088e9fdbf52e210fc76813b965435480
Mannuru Nikhitha <mannurunikhitha@gmail.com>
2026-04-07 20:14:01 +0530

    69260

--- home/urls.py
+++ home/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('api/',include(router.urls)),
     path('tables/<int:pk>/', TableDetailView.as_view(), name='table-detail'),
     path('check-email/', check_email),
+    path('api/tables/available/', AvailableTablesAPIView.as_view(), name='available_table_api'),
 ]
