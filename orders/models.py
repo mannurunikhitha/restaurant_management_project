cc190c64b05cefc359ab9fafe644582230516921
Mannuru Nikhitha <mannurunikhitha@gmail.com>
2026-04-13 19:32:24 +0530

    69849

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from .utils import generate_unique_order_id
+from home.models import MenuItem
+from decimal import Decimal
 
 # Create your models here.
 class OrderStatus(models.Model):
@@ -40,6 +42,13 @@ class OrderItem(models.Model):
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
     menu_item = models.ForeignKey(Order, on_delete=models.CASCADE)
+    def calculate_total(self):
+        total = Decimal('0.00')
+        for item in self.items.all():
+            total += item.price * item.quantity
+        return total
+    def __str__(self):
+        return f"{self.menu_item.name} x {self.quantity}"
 
 class OrderManager(models.Manager):
     def get_active_orders(self):
