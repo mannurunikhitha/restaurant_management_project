f108e9fb50c415991adefa43db0bf3c046a86932
Mannuru Nikhitha <mannurunikhitha@gmail.com>
2026-04-13 18:50:46 +0530

    69846

--- home/models.py
+++ home/models.py
@@ -92,3 +92,11 @@ class Ingredient(models.Model):
     def __str__(self):
         return self.name
 
+class ContactFormSubmission(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.name}-{self.email}"
+
